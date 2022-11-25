2b3b6d6adf680a58e21075f712145dd3f4437994
Ali ibrahim jasim <ali.ibrahim.jasim@gmail.com>
2022-11-25 22:47:17 +0300

    Completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,29 @@
 import React from "react";
+// import user from "../data/user";
+import Links from "./Links"
 
-function About() {
+//  const propsOfBio = props.bio
+
+function About(props ) {
+  
+  function IsAppear(){
+    if(props.bio){
+      return (<p>{props.bio}</p>)
+    }else if (props.bio === "" && props.bio === null) {
+      return (<p>{null}</p>)
+    }
+  }
+  
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {/* {props.bio && props.bio !== "" && <p>{props.bio}</p>} the above solution also work But this one is more concise  */}
+     { IsAppear()}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links 
+      linkedin={props.linkedin}
+      github={props.github}
+      />
     </div>
   );
 }
